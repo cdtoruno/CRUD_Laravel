d7e1eb239cd3806b23f9725aa99ad52102472bba
Carlos Diego Toruño Espinales <cdtoruno@uamv.edu.ni>
2024-06-10 00:47:37 -0600

    Crud Laravel Final

--- app/Http/Controllers/ControllerCareer.php
+++ app/Http/Controllers/ControllerCareer.php
@@ -17,52 +17,54 @@ class ControllerCareer extends Controller
         return view('career.index', compact('careers'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
-        //
         return view('careers.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        
+        $career = new Career();
+        $career->codigo = $request->codigo;
+        $career->nombre = $request->nombre;
+        $career->descripcion = $request->descripcion;
+        $career->save();
+        return redirect()->route('careers.index');
     }
 
-    /**
-     * Display the specified resource.
-     */
+
     public function show(string $id)
     {
-        //
+        
+        $career = Career::find($id);
+        return view('careers.show', compact('career'));
+    
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
+    
     public function edit(string $id)
     {
-        //
+        $career = Career::find($id);
+        return view('careers.edit', compact('career'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+    
     public function update(Request $request, string $id)
     {
-        //
+            $career = Career::find($id);
+            $career->codigo = $request->codigo;
+            $career->nombre = $request->nombre;
+            $career->descripcion = $request->descripcion;
+            $career->save();
+            return redirect()->route('careers.index');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+
     public function destroy(string $id)
     {
-        //
+        $career = Career::find($id);
+        $career->delete();
+        return redirect()->route('careers.index');
     }
 }
